1b301b234e62767942b954297490c8c2c508dd00
raghadalquran <raghadalquran1@gmail.com>
2020-03-30 18:46:13 +0300

    still working2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+let arrayToPush=[];
 $(document).ready(function(){
   function Gallery(image_url,title,description,keyword,horns){
     this.image_url = image_url;
@@ -12,17 +13,34 @@ $(document).ready(function(){
     $galleryClone.find('img').attr('src',this.image_url);
     $galleryClone.find('h2').text(this.title);
     $galleryClone.find('p').text(this.description);
-    $('main section').append($galleryClone);
+    $galleryClone.attr('class',this.keyword);
+    $('main').append($galleryClone);
   };
-  const page1Json = () => {
-    $.ajax('../data/page-1.json', { method: 'GET' ,dataType:'JSON'}).then(page1 => {
-      page1.forEach(galleryItem => {
-        let gallery = new Gallery(galleryItem);
+  Gallery.prototype.filter = function(){
+    let imageFilter = $('.select-class');
+    if(!(arrayToPush.includes(this.keyword))){
+      arrayToPush.push(this.keyword);
+      imageFilter.append(`<option>${this.keyword}</option>`);}
+  };
+  const pageJson = () => {
+    $.ajax('../data/page-1.json', { method: 'GET' ,dataType: 'JSON'}).then(page1 => {
+      page1.forEach(function(value){
+        let gallery = new Gallery(value.image_url,value.title,value.description,value.keyword,value.horns);
+        gallery.filter();
         gallery.render();
       });
     });
   };
-  page1Json();
+  pageJson();
 });
 
 
+$('.select-class').change(function(){
+  var selected = $(this).children('option:selected').val();
+  arrayToPush.forEach(function(val){
+    if(selected === val) {
+      $('section').hide();
+      $(`.${val}`).show();
+    }
+  });
+});
